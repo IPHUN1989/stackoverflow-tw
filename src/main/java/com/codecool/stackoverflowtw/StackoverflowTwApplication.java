dcb1e631dee33df8f738bb361b5db1a167d4b527
IPHUN1989 <tamas.peter.illes@gmail.com>
2023-08-02 13:32:23 +0200

    refactor(StackoverflowTwApplication.java ): Add new parameter to QuestionsDaoJdbc constructor
    Add new parameter to QuestionsDaoJdbc constructor

--- src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
+++ src/main/java/com/codecool/stackoverflowtw/StackoverflowTwApplication.java
@@ -2,6 +2,7 @@ package com.codecool.stackoverflowtw;
 
 import com.codecool.stackoverflowtw.dao.QuestionsDAO;
 import com.codecool.stackoverflowtw.dao.QuestionsDaoJdbc;
+import com.codecool.stackoverflowtw.dao.conneciton.DatabaseConnectionProviderImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +12,11 @@ public class StackoverflowTwApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StackoverflowTwApplication.class, args);
+
     }
 
     @Bean
     public QuestionsDAO questionsDAO() {
-        return new QuestionsDaoJdbc();
+        return new QuestionsDaoJdbc(new DatabaseConnectionProviderImpl());
     }
 }
